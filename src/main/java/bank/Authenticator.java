10f63b26cdf15d45561d0d33c505fb61bb484c81
Muhydeen Adelani <139554441+DataNaija@users.noreply.github.com>
2024-03-02 09:39:08 +0000

    third commit

--- src/main/java/bank/Authenticator.java
+++ src/main/java/bank/Authenticator.java
@@ -3,9 +3,10 @@ package bank;
 import javax.security.auth.login.LoginException;
 
 public class Authenticator {
-  public static Customer login(String username, String password, throw LoginException){
+  @SuppressWarnings("null")
+  public static Customer login(String username, String password) throws LoginException {
     Customer customer = DataSource.getCustomer(username);
-    if(customer = null){
+    if(customer != null){
       throw new LoginException("Username not found");
     }
 
